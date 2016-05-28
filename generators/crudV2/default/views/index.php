40aba38fc9ddb25f98247676d4238eab76447a5b
cakpep <febrimaschut@gmail.com>
2016-05-28 20:30:38 +0700

    update model name on view index

--- generators/crudV2/default/views/index.php
+++ generators/crudV2/default/views/index.php
@@ -1,9 +1,13 @@
 <?php
+use yii\helpers\StringHelper;
 /* @var $this yii\web\View */
 /* @var $generator yii\gii\generators\crud\Generator */
-
 /* @var $model \yii\db\ActiveRecord */
+
 $model = new $generator->modelClass();
+$controllerClass = StringHelper::basename($generator->controllerClass);
+$controllerClass = str_replace('controller', '',$controllerClass);
+$modelClass = StringHelper::basename($generator->modelClass);
 $safeAttributes = $model->safeAttributes();
 if (empty($safeAttributes)) {
     $safeAttributes = $model->attributes();
@@ -13,14 +17,14 @@ echo "<?php\n";
 ?>
 use yii\helpers\Url;
 /* @var $this yii\web\View */
-/* @var $model <?= ltrim($generator->modelClass, '\\') ?> */
+/* @var $model <?= strtolower($modelClass); ?> */
 ?>
-<!-- Data Grid ( #dg-<?= ltrim($generator->modelClass, '\\') ?> ) -->
+<!-- Data Grid ( #dg-<?= strtolower($modelClass); ?> ) -->
 <div style="padding:1px">
-	<table id="dg-<?= ltrim($generator->modelClass, '\\') ?>" title="<?= ltrim($generator->modelClass, '\\') ?>"
+	<table id="dg-<?= strtolower($modelClass); ?>" title="<?= strtolower($modelClass); ?>"
 		class="easyui-datagrid" width="auto" height="auto"
-		url="<?php echo  '<?='; ?> Url::to(['<?= strtolower(ltrim($generator->modelClass, '\\')) ?>/data']) ?>"
-		toolbar="#tb-<?= ltrim($generator->modelClass, '\\') ?>" pagination="true"
+		url="<?php echo  '<?='; ?> Url::to(['data']) ?>"
+		toolbar="#tb-<?= strtolower($modelClass); ?>" pagination="true"
 		rownumbers="true" fitColumns="true"
 		singleSelect="true" collapsible="true"
 	>
@@ -39,17 +43,17 @@ use yii\helpers\Url;
 </div>
 
 
-<!-- Toolbar Data Grid ( #tb-<?= ltrim($generator->modelClass, '\\') ?> ) -->
-<div id="tb-<?= ltrim($generator->modelClass, '\\') ?>">
-	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-add" plain="true" onClick="create_data('#df-<?= ltrim($generator->modelClass, '\\') ?>','#f-<?= ltrim($generator->modelClass, '\\') ?>','Create <?= ltrim($generator->modelClass, '\\') ?>','<?php echo  '<?='; ?> Url::to(['<?= ltrim($generator->modelClass, '\\') ?>/update']) ?>')">Create</a>
-	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-edit" plain="true" onClick="update_data('#df-<?= ltrim($generator->modelClass, '\\') ?>','#f-<?= ltrim($generator->modelClass, '\\') ?>','Update <?= ltrim($generator->modelClass, '\\') ?>','#dg-<?= ltrim($generator->modelClass, '\\') ?>','<?= Url::to(['<?= ltrim($generator->modelClass, '\\') ?>/update']) ?>')">Update</a>
-	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-remove" plain="true" onClick="remove_data('#dg-<?= ltrim($generator->modelClass, '\\') ?>','<?php echo  '<?='; ?> Url::to(['<?= ltrim($generator->modelClass, '\\') ?>/delete']) ?>')">Delete</a>
-	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-search" plain="true" onClick="search_dialog('#ds-<?= ltrim($generator->modelClass, '\\') ?>')">Search</a>
+<!-- Toolbar Data Grid ( #tb-<?= strtolower($modelClass); ?> ) -->
+<div id="tb-<?= strtolower($modelClass); ?>">
+	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-add" plain="true" onClick="create_data('#df-<?= strtolower($modelClass); ?>','#f-<?= strtolower($modelClass); ?>','Create <?= strtolower($modelClass); ?>','<?php echo  '<?='; ?> Url::to(['update']) ?>')">Create</a>
+	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-edit" plain="true" onClick="update_data('#df-<?= strtolower($modelClass); ?>','#f-<?= strtolower($modelClass); ?>','Update <?= strtolower($modelClass); ?>','#dg-<?= strtolower($modelClass); ?>','<?php echo  '<?='; ?> Url::to(['update']) ?>')">Update</a>
+	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-remove" plain="true" onClick="remove_data('#dg-<?= strtolower($modelClass); ?>','<?php echo  '<?='; ?> Url::to(['delete']) ?>')">Delete</a>
+	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-search" plain="true" onClick="search_dialog('#ds-<?= strtolower($modelClass); ?>')">Search</a>
 </div>
 
-<!-- Dialog Form ( #df-<?= ltrim($generator->modelClass, '\\') ?> ) -->
-<div id="df-<?= ltrim($generator->modelClass, '\\') ?>" class="easyui-dialog" style="width: 400px; height: 240px; padding: 10px 20px" closed="true" modal="true" buttons="#bf-<?= ltrim($generator->modelClass, '\\') ?>">
-	<form id="f-<?= ltrim($generator->modelClass, '\\') ?>" method="post" novalidate onSubmit="return false">
+<!-- Dialog Form ( #df-<?= strtolower($modelClass); ?> ) -->
+<div id="df-<?= strtolower($modelClass); ?>" class="easyui-dialog" style="width: 400px; height: 240px; padding: 10px 20px" closed="true" modal="true" buttons="#bf-<?= strtolower($modelClass); ?>">
+	<form id="f-<?= strtolower($modelClass); ?>" method="post" novalidate onSubmit="return false">
 		<div class="form-item">
 			<label for="type">Type</label><br />
 			<input type="text" name="type" class="easyui-validatebox" required="true" maxlength="50" size="53" />
@@ -75,20 +79,20 @@ use yii\helpers\Url;
 	</form>
 </div>
 
-<!-- Button Form ( #bf-<?= ltrim($generator->modelClass, '\\') ?> ) -->
-<div id="bf-<?= ltrim($generator->modelClass, '\\') ?>">
-	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-save" onClick="save_data('#f-<?= ltrim($generator->modelClass, '\\') ?>','#df-<?= ltrim($generator->modelClass, '\\') ?>','#dg-<?= ltrim($generator->modelClass, '\\') ?>')">Simpan</a>
-	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-cancel" onClick="javascript:jQuery('#df-<?= ltrim($generator->modelClass, '\\') ?>').dialog('close')">Batal</a>
+<!-- Button Form ( #bf-<?= strtolower($modelClass); ?> ) -->
+<div id="bf-<?= strtolower($modelClass); ?>">
+	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-save" onClick="save_data('#f-<?= strtolower($modelClass); ?>','#df-<?= strtolower($modelClass); ?>','#dg-<?= strtolower($modelClass); ?>')">Simpan</a>
+	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-cancel" onClick="javascript:jQuery('#df-<?= strtolower($modelClass); ?>').dialog('close')">Batal</a>
 </div>
 
-<!-- Dialog Search ( #ds-<?= ltrim($generator->modelClass, '\\') ?> ) -->
-<div id="ds-<?= ltrim($generator->modelClass, '\\') ?>" class="easyui-dialog" style="width:400px; height:240px; padding: 10px 20px" closed="true" buttons="#bs-<?= ltrim($generator->modelClass, '\\') ?>" iconCls="IconMagnifier" >
+<!-- Dialog Search ( #ds-<?= strtolower($modelClass); ?> ) -->
+<div id="ds-<?= strtolower($modelClass); ?>" class="easyui-dialog" style="width:400px; height:240px; padding: 10px 20px" closed="true" buttons="#bs-<?= strtolower($modelClass); ?>" iconCls="IconMagnifier" >
 	<?php foreach ($generator->getColumnNames() as $attribute) {
 			if (in_array($attribute, $safeAttributes)) {
 	?>
 		<div class="form-item">
-			<label for="search_<?= ltrim($generator->modelClass, '\\') ?>_<?= $attribute ?>"><?= $generator->getAttributeLabel($attribute) ?></label>
-			<input type="text" name="search_<?= ltrim($generator->modelClass, '\\') ?>_<?= $attribute ?>" id="fs-<?= ltrim($generator->modelClass, '\\') ?>-<?= $attribute ?>" size="52" class="easyui-textbox" />
+			<label for="search_<?= strtolower($modelClass); ?>_<?= $attribute ?>"><?= $generator->getAttributeLabel($attribute) ?></label>
+			<input type="text" name="search_<?= strtolower($modelClass); ?>_<?= $attribute ?>" id="fs-<?= strtolower($modelClass); ?>-<?= $attribute ?>" size="52" class="easyui-textbox" />
 		</div>
 	<?php
 			}
@@ -96,8 +100,8 @@ use yii\helpers\Url;
 
 </div>
 
-<!-- Button Search ( #bs-<?= ltrim($generator->modelClass, '\\') ?> ) -->
-<div id="bs-<?= ltrim($generator->modelClass, '\\') ?>">
-	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-reload" onClick="search_data_<?= ltrim($generator->modelClass, '\\') ?>()">Cari</a>
-	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-cancel" onClick="javascript:jQuery('#ds-<?= ltrim($generator->modelClass, '\\') ?>').dialog('close')">Tutup</a>
+<!-- Button Search ( #bs-<?= strtolower($modelClass); ?> ) -->
+<div id="bs-<?= strtolower($modelClass); ?>">
+	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-reload" onClick="search_data_<?= strtolower($modelClass); ?>()">Cari</a>
+	<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-cancel" onClick="javascript:jQuery('#ds-<?= strtolower($modelClass); ?>').dialog('close')">Tutup</a>
 </div>
