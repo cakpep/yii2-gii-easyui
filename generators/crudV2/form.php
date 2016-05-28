fa628d23d5f4fdab8a7eeddd9940fe081b1b93e8
cakpep <febrimaschut@gmail.com>
2016-05-28 15:16:12 +0700

    new template

---
+++ generators/crudV2/form.php
@@ -0,0 +1,10 @@
+<?php
+/* @var $this yii\web\View */
+/* @var $form yii\widgets\ActiveForm */
+/* @var $generator cakpep\giieasyui\generators\crud\Generator */
+
+echo $form->field($generator, 'modelClass');
+echo $form->field($generator, 'searchModelClass');
+echo $form->field($generator, 'controllerID');
+echo $form->field($generator, 'baseControllerClass');
+echo $form->field($generator, 'messageCategory');
